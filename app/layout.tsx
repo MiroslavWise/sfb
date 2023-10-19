e5cea08aab0dbd8c104a09fe6449af25e2b4ce53
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-10-19 19:35:34 +0300

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const Provider = dynamic(() => import("@/context/provider"), { ssr: false })
 
 import { cx } from "@/helpers/lib/cx"
 
-import "@/scss/init.scss"
+import "@/scss/init.scss" 
 import "@coreui/coreui/dist/css/coreui.min.css"
 
 const inter = Inter({ subsets: ["latin"] })
