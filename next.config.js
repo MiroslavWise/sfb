d4528105d056ac2e01a19f2e5470fc80a2943c07
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-11-24 20:52:53 +0300

    aasdf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        formats: ["image/avif", "image/webp"],
         domains: [process.env.NEXT_PUBLIC_DOMAINS],
     },
     swcMinify: true,
