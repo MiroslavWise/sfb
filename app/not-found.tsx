b2caad2ec36fa5167d17528afb809919522d7c94
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-12-07 19:26:38 +0300

    not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function NotFound() {
     return (
         <div className="__not-found__">
             <h3>Эта станица не доступна, или вы не правильный адрес ввели</h3>
+            <Link href="/">
+                <h4>Вернуться на домашнюю страницу</h4>
+            </Link>
         </div>
     )
 }
